74cd914e23a891549e809a038b4ac17a15a29f04
dcsalengue <dcsalengue@me.com>
2024-11-30 23:00:36 -0300

    Aula 4

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -19,20 +19,22 @@ class Formulario extends React.Component {
                     />
                 </div>
                 <div className={style.inputContainer}>
-                <label htmlFor="tempo">`
-                Tempo
-              </label>
-              <input
-                type="time"
-                step="1"
-                name="tempo"
-                id="tempo"
-                min="00:00:00"
-                max="01:30:00"
-                required
-              />
+                    <label htmlFor="tempo">`
+                        Tempo
+                    </label>
+                    <input
+                        type="time"
+                        step="1"
+                        name="tempo"
+                        id="tempo"
+                        min="00:00:00"
+                        max="01:30:00"
+                        required
+                    />
                 </div>
-                <Botao />
+                <Botao>
+                    Adicionar
+                </Botao>
             </form>
         )
     }
